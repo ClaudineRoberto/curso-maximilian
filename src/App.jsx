97f0176c2e5219953e885bdac355bc253b78c887
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-02-15 13:26:44 -0300

    condição de exibição alternativa

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,21 @@ function App() {
     setSelectedTopic(selectedButton);
   }
 
+  let tabContent = <p>Click on a tab to view the example</p>
+
+  if (selectedTopic) {
+    tabContent =
+      <div id="tab-content">
+        <h3> {EXAMPLES[selectedTopic].title} </h3>
+        <p> {EXAMPLES[selectedTopic].description} </p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+  }
+
   return (
     <div>
       <Header />
@@ -33,17 +48,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {!selectedTopic ? <p>Click on a tab to view the example</p> :
-            <div id="tab-content">
-              <h3> {EXAMPLES[selectedTopic].title} </h3>
-              <p> {EXAMPLES[selectedTopic].description} </p>
-              <pre>
-                <code>
-                  {EXAMPLES[selectedTopic].code}
-                </code>
-              </pre>
-            </div>
-          }
+          {tabContent}
         </section>
       </main>
     </div>
